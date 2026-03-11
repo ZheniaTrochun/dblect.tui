b6cf30f4199784da400782bd3c1b009537d97ced
Yevhenii Trochun <yevhenii.trochun@lotusflare.com>
2026-03-11 16:40:52 +0200

    restructuring

---
+++ web/dblect_tui-web-backend/app.js
@@ -0,0 +1,58 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+    cors: { origin: "http://localhost:5173" }
+});
+
+const { Client: SSHClient } = require('ssh2');
+
+const SSH_HOST = 'localhost';
+const SSH_PORT = 23234;
+const SERVER_PORT = 5174;
+
+io.on('connection', socket => {
+    console.log('Frontend connected, opening SSH tunnel...');
+
+    const cols = parseInt(socket.handshake.query.cols) || 80;
+    const rows = parseInt(socket.handshake.query.rows) || 24;
+
+    const ssh = new SSHClient();
+
+    ssh
+        .on('banner', msg => console.log('SSH banner:', msg))
+        .on('close', () => console.log('SSH close'))
+        .on('end', () => console.log('SSH end'))
+        .on('error', err => console.error('SSH error:', err))
+        .on('ready', () => {
+            socket.emit('data', '\r\n*** SSH CONNECTION ESTABLISHED ***\r\n');
+            ssh.shell({ term: 'xterm-256color', cols, rows }, (err, stream) => {
+                if (err) {
+                    socket.emit('data', '\r\n*** SSH SHELL ERROR: ' + err.message + ' ***\r\n');
+                    return;
+                }
+
+                socket.on('data', data => stream.write(data));
+                socket.on('resize', ({ cols, rows }) => stream.setWindow(rows, cols, 0, 0));
+
+                stream
+                    .on('data', data => socket.emit('data', data))
+                    .on('close', () => ssh.end());
+            });
+        })
+        .connect({
+            host: SSH_HOST,
+            port: SSH_PORT,
+            username: 'user',
+            password: 'asd123',
+        });
+
+    socket.on('disconnect', () => {
+        console.log('Frontend disconnected, closing SSH tunnel.');
+        ssh.end();
+    });
+});
+
+server.listen(SERVER_PORT, () => {
+    console.log(`App ready on :${SERVER_PORT}`);
+});
