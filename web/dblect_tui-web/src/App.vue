90010a83c94c1f83943e5ed7b2de45d614bb8ce6
Yevhenii Trochun <yevhenii.trochun@lotusflare.com>
2026-03-14 13:16:16 +0200

    front-end refactor

--- web/dblect_tui-web/src/App.vue
+++ web/dblect_tui-web/src/App.vue
@@ -2,45 +2,81 @@
   import {Terminal} from "@xterm/xterm";
   import '@xterm/xterm/css/xterm.css';
   import { FitAddon } from '@xterm/addon-fit';
-  import {onMounted} from "vue";
+  import {onMounted, onUnmounted, ref} from "vue";
   import {io} from 'socket.io-client'
 
+  const terminalContainer = ref(null)
+
+  const term = new Terminal({ cursorBlink: true, theme: { background: '#333' }, lineHeight: 1, fontSize: 14})
+  const fitAddon = new FitAddon()
+  term.loadAddon(fitAddon)
+
+  let socket
+  let backgroundReconnect
+
   onMounted(() => {
+    term.open(terminalContainer.value)
+    fitAddon.fit()
+    window.addEventListener('resize', () => fitAddon.fit())
 
-    const term = new Terminal({ cursorBlink: true, theme: { background: '#333' }, lineHeight: 1, fontSize: 14});
-    const fitAddon = new FitAddon();
-    term.loadAddon(fitAddon);
-    const terminalContainer = document.getElementById('terminal-container');
-    term.open(terminalContainer);
-    fitAddon.fit();
+    socket = setupSocket(term)
 
-    const socket = io({ autoConnect: false, query: { cols: term.cols, rows: term.rows } });
-
-    term.onResize(({ cols, rows }) => socket.emit('resize', { cols, rows }));
-    window.addEventListener('resize', () => fitAddon.fit());
-
-    term.onData(data => socket.emit('data', data));
-
-    socket.on('data', data => {
-      if (data instanceof ArrayBuffer) {
-        term.write(new Uint8Array(data));
-      } else {
-        term.write(data);
-      }
-    });
-
-    socket.on('disconnect', () => {
-      term.clear()
-      term.write("Disconnected")
-    });
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected, retry connection in 1 sec")
+      backgroundReconnect = setTimeout(() => {
+        console.log("Retrying socket connection")
+        socket = setupSocket(term)
+        socket.connect()
+      }, 1000)
+    })
 
     term.focus();
     socket.connect();
   })
+
+  onUnmounted(() => {
+    if (socket) {
+      socket.disconnect()
+    }
+
+    if (backgroundReconnect) {
+      clearTimeout(backgroundReconnect)
+    }
+
+    term.dispose()
+  })
+
+  function setupSocket(term) {
+    const socket = io({ autoConnect: false, query: { cols: term.cols, rows: term.rows } })
+
+    term.onResize(({ cols, rows }) => socket.emit('resize', { cols, rows }))
+    term.onData(data => socket.emit('data', data))
+
+    socket.on('data', data => {
+      if (data instanceof ArrayBuffer) {
+        term.write(new Uint8Array(data))
+      } else {
+        term.write(data)
+      }
+    })
+
+    socket.on("connect_error", (err) => {
+      console.error(`Connection failed: ${err.message}`)
+
+      term.write(`\r\nConnection failed: ${err.message}\r\n`)
+    })
+
+    socket.on('disconnect', () => {
+      term.clear()
+      term.write("Connection lost, retrying...")
+    })
+
+    return socket
+  }
 </script>
 
 <template>
-    <div id="terminal-container"></div>
+    <div id="terminal-container" ref="terminalContainer"></div>
 </template>
 
 <style>
